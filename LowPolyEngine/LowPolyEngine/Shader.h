4359524d46c322ea8a46adbbbd1f1fd6d56582c0
Jannik Petersen <glutexo@live.de>
2019-11-07 14:06:01 +0100

    [ADD] Shader Vertex, Fragment

---
+++ LowPolyEngine/LowPolyEngine/Shader.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
+//#include "defines.h"
+
+struct Shader
+{
+	Shader(const char* vertexShaderFilename, const char* fragmentShaderFilename);
+
+	virtual ~Shader();
+
+	void bind();
+	void unbind();
+
+private:
+
+	GLuint compile(std::string shaderSource, GLenum type);
+	std::string parse(const char* filename);
+	GLuint createShader(const char* vertexShaderFilename, const char* fragmentShaderFilename);
+
+	GLuint shaderID;
+};
+
